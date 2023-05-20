6e65f1562296550024f1bfd49aa57a65d5cc38ba
Isaac2109 <lrsiazevedo2018@gmail.com>
2023-05-20 15:53:14 -0300

    Create numbers_pygame.py
    Este projeto se trata do mesmo mini game, só que com algumas melhorias e feito usando a biblioteca pygame.

---
+++ numbers_pygame.py
@@ -0,0 +1,136 @@
+import os
+import pygame as pg, random
+from pygame.locals import *
+
+# imagem_number = pygame.image.load(os.path.join('imagem_nubers1.jpg'))
+clock = pg.time.Clock()
+pg.init()
+tela = pg.display.set_mode((400,500))
+pg.display.set_caption("Numbers")
+
+dica = 0
+numero_sorteado = 0
+font = pg.font.Font(None, 32)
+input_box = pg.Rect(100,150,140,32)
+color_inactive = pg.Color('lightskyblue3')
+color_active = pg.Color('dodgerblue2')
+color = color_inactive
+active = False
+text = ''
+
+
+font_text = pg.font.SysFont('arial', 25)
+running = True
+
+cor_background = 37, 247, 104
+
+def sortear(lista):
+    global numero_sorteado
+    sorteado = random.choice(lista)
+    numero_sorteado = sorteado
+    print(numero_sorteado)
+
+
+def check(numero_certo, palpite):
+    text_dica_menor = font_text.render("O número sorteado é menor do que seu palpite", 1, (0, 50, 150))
+    text_dica_maior = font_text.render("O número sorteado é maior do que seu palpite", 1, (0, 50, 150))
+    text_acertou = font_text.render("vc acertou", 1, (0, 50, 150))
+    if numero_certo > int(palpite):
+        tela.blit(text_dica_maior, (60,260))
+    if numero_certo < int(palpite):
+        tela.blit(text_dica_menor, (60,260))
+    if numero_certo == int(palpite):
+        tela.blit(text_acertou, (60,260))
+    
+
+
+# tela inicial
+tela_inicial = True
+text_inicial_1 = font_text.render("Este mini-game consiste em acertar", 1, (0, 50, 150))
+text_inicial_2 = font_text.render("dentro do tempo estipulado", 1, (0, 50, 150))
+text_inicial_3 = font_text.render("um número que foi sorteado entre 0 e 100", 1, (0, 50, 150))
+text_inicial_4 = font_text.render("a cada acerto o tempo diminui", 1, (0, 50, 150))
+text_inicial_5 = font_text.render("tente acertar o máximo de números", 1, (0, 50, 150))
+text_inicial_6 = font_text.render("que você puder antes que o tempo acabe", 1, (0, 50, 150))
+text_inicial_7 = font_text.render("para começar pressione espaço", 1, (10,50,250))
+
+# jogo rodando
+playing = False
+text_palpite = font_text.render("Digite seu palpite no campo abaixo", 1, (0, 50, 150))
+
+
+# tela restart
+tela_restart = False
+
+while running:
+    clock.tick(60)
+    tela.fill(cor_background)
+    # tela.blit(imagem_number, (0,0))
+    if tela_inicial:
+        tela.blit(text_inicial_1, (40,50))
+        tela.blit(text_inicial_2, (80,80))
+        tela.blit(text_inicial_3, (12,110))
+        tela.blit(text_inicial_4, (70,140))
+        tela.blit(text_inicial_5, (45,170))
+        tela.blit(text_inicial_6, (15,200))
+        tela.blit(text_inicial_7, (60,260))
+        
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == K_SPACE:
+                    tela_inicial = False
+                    playing = True
+                    sortear(range(100))
+                    
+        
+
+    if playing:
+        tela.blit(text_palpite, (45,100))
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if input_box.collidepoint(event.pos):
+                    active = not active
+                else:
+                    active = False
+                color = color_active if active else color_inactive
+            if event.type == pg.KEYDOWN:
+                if active:
+                    if event.key == pg.K_RETURN:
+                        while not event.key == pg.K_RETURN:
+                            check(numero_sorteado, text)
+                        
+                        text = ''
+                    elif event.key == pg.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
+                   
+        txt_surface = font.render(text, True, color)
+    
+        width = max(200, txt_surface.get_width()+10)
+        input_box.w = width
+        tela.blit(txt_surface, (input_box.x+5, input_box.y+5))
+        pg.draw.rect(tela, color, input_box, 2)
+
+
+
+
+    if tela_restart:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+    
+
+    
+    pg.display.update()
+
+
+
+
+
